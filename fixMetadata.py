55665094b6b2db4c69db5fe9c2f0e35cc58e6a7f
Aidan <akinzett@gmail.com>
2020-03-09 12:39:44 +1000

    added script files

--- fixMetadata.py
+++ fixMetadata.py
@@ -10,17 +10,14 @@ print(FOLDER_PATH)
 thermal_images = [
     join(FOLDER_PATH, f)
     for f in listdir(FOLDER_PATH)
-    if isfile(join(FOLDER_PATH, f))
-    and not f.startswith(".")
+    if isfile(join(FOLDER_PATH, f)) and not f.startswith(".")
 ]
 
 print(thermal_images)
 
 for image in thermal_images:
     filename = join(
-        FOLDER_PATH,
-        "bwr",
-        "".join(image.split("/")[-1])[:-4] + "_thermal_bwr.jpg",
+        FOLDER_PATH, "bwr", "".join(image.split("/")[-1])[:-4] + "_thermal_bwr.jpg"
     )
     print(filename)
     subprocess.run(["exiftool", "-tagsfromfile", image, filename])
--- multipleImages.py
+++ multipleImages.py
@@ -60,4 +60,5 @@ def multiple_images(FOLDER_PATH):
         flir.process_image(image)
         flir.save_images(minTemp=dataset_min, maxTemp=dataset_max)
 
+
 multiple_images("/Volumes/Jaimyn USB/veolia/thermal")
