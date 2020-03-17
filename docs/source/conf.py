b4af21741683b5db4bb0fb729819bc9f877cf761
Aidan <akinzett@gmail.com>
2020-03-17 12:46:40 +1000

    Bump version: 1.3.0 → 1.4.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2019, National Drones"
 author = "National Drones"
 
 # The full version, including alpha/beta/rc tags
-release = "1.3.0"
+release = "1.4.0"
 master_doc = "index"
 
 # -- General configuration ---------------------------------------------------
