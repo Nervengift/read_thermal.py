e076807f21c6a02a41f5f69abad92e9442c3e2ae
Conor <conorbrosnan10@gmail.com>
2020-04-16 13:32:42 +1000

    Add dependency to setup

--- setup.py
+++ setup.py
@@ -23,5 +23,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["numpy", "pillow", "matplotlib"],
+    install_requires=["numpy", "pillow", "matplotlib", "loguru"],
 )
